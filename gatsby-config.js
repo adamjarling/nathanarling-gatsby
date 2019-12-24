f653fad6855add93ff51aaffdd5e1fca485061fa
Adam Arling <aarling@gmail.com>
2019-12-24 09:35:18 -0600

    Homepage cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,9 @@
 module.exports = {
   siteMetadata: {
     title: 'Nathan Arling - drummer, talent agent and musician',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    author: 'Adam J. Arling',
+    description:
+      'Developed by Adam J. Arling and based on Dimension by HTML5 UP',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,7 +16,7 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/gatsby-icon.png', // This path is relative to the root of the site.
+        icon: 'src/images/na-icon.png', // This path is relative to the root of the site.
       },
     },
     'gatsby-plugin-sass',
