f9497a95db826797127687106da009d92ae00961
Adam Arling <adam.arling@northwestern.edu>
2020-02-09 13:48:15 -0600

    Update footer info

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,8 +4,9 @@ import PropTypes from 'prop-types'
 const Footer = props => (
   <footer id="footer" style={props.timeout ? { display: 'none' } : {}}>
     <p className="copyright">
-      &copy; Dimension. Design: <a href="https://html5up.net">HTML5 UP</a>.
-      Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
+      &copy; Dimension. Design:{' '}
+      <a href="https://adamarling.com">Site by Adam J. Arling</a>. Built with:{' '}
+      <a href="https://www.gatsbyjs.org/">Gatsby.js</a>
     </p>
   </footer>
 )
